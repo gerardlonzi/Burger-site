c1c9b6304f9d52c017a99441f1ac60684d863067
gerardlonzi <lonzigerard@gmail.com>
2023-08-18 22:36:21 +0200

    barner part

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import HomePages from './pages/HomePages'
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+    <HomePages />
+    </>
   )
 }
---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Barner from '../components/barner'
+export default function HomePages() {
+  return (
+    <>
+    <Barner />
+    </>
+  )
+}
